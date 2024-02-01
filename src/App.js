480838b34a5fdc7b3eadf81d3f0f2abb33b3be52
Шагунов Арсений <279014135@TOP.ACADEMY>
2024-02-01 21:16:46 +0300

    DZ

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import Movies from './components/Movies';
 import { useState, useEffect } from 'react';
 import { APIKEY } from './env.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchData } from  './actions'
 
 import './App.scss';
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
   const [searchField, setSearchField] = useState('');
   const URL = `https://www.omdbapi.com/?s=${searchField}&apikey=${APIKEY}`;
+  const movies = useSelector(state => state.movies);
+  const dispatch = useDispatch();
 
   function getMovies() {
-    fetch(encodeURI(URL))
-      .then(res => res.json())
-      .then(movies => setMovies(movies.Search))
+  //   fetch(encodeURI(URL))
+  //     .then(res => res.json())
+  //     .then(movies => setMovies(movies.Search))
+  // }
+    dispatch (fetchData(URL))
   }
 
-  useEffect(getMovies, []);
+  useEffect(getMovies, [dispatch]);
   useEffect(getMovies, [searchField]);
   
   let timer;
@@ -30,7 +36,7 @@ function App() {
   return (
     <div className="App">
       <input type="text" onChange={handleChange} />
-      <Movies movies={movies} />
+      <Movies movies={movies.movies} />
     </div>
   );
 }
